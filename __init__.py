5923d0c1769d781bad5c1fbc0159566abbb9a06e
sbssai123 <sbs.sai@gmail.com>
2019-02-09 21:27:35 -0500

    added index page

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 ##
 # Flask Drive Example App
 #
@@ -16,37 +17,35 @@ from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
+
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-	credentials = get_credentials()
-	if credentials == False:
-		return flask.redirect(flask.url_for('oauth2callback'))
-	elif credentials.access_token_expired:
-		return flask.redirect(flask.url_for('oauth2callback'))
-	else:
-		print('now calling fetch')
-		all_files = fetch("'root' in parents and mimeType = 'application/vnd.google-apps.folder'", sort='modifiedTime desc')
-		s = ""
-		for file in all_files:
-			s += "%s, %s<br>" % (file['name'],file['id'])
-		return s
+    credentials = get_credentials()
+    if credentials == False:
+        return flask.redirect(flask.url_for('oauth2callback'))
+    elif credentials.access_token_expired:
+        return flask.redirect(flask.url_for('oauth2callback'))
+    else:
+        print('now calling fetch')
+        all_files = fetch("'root' in parents and mimeType = 'application/vnd.google-apps.folder'", sort='modifiedTime desc')
+    return flask.render_template("index.html", files=all_files)
 		
 @app.route('/oauth2callback')
 def oauth2callback():
-	flow = client.flow_from_clientsecrets('client_id.json',
-			scope='https://www.googleapis.com/auth/drive',
-			redirect_uri=flask.url_for('oauth2callback', _external=True)) # access drive api using developer credentials
-	flow.params['include_granted_scopes'] = 'true'
-	if 'code' not in flask.request.args:
-		auth_uri = flow.step1_get_authorize_url()
-		return flask.redirect(auth_uri)
-	else:
-		auth_code = flask.request.args.get('code')
-		credentials = flow.step2_exchange(auth_code)
-		open('credentials.json','w').write(credentials.to_json()) # write access token to credentials.json locally 
-		return flask.redirect(flask.url_for('index'))
+    flow = client.flow_from_clientsecrets('client_id.json',
+	        scope='https://www.googleapis.com/auth/drive',
+            redirect_uri=flask.url_for('oauth2callback', _external=True)) # access drive api using developer credentials
+    flow.params['include_granted_scopes'] = 'true'
+    if 'code' not in flask.request.args:
+        auth_uri = flow.step1_get_authorize_url()
+        return flask.redirect(auth_uri)
+    else:
+        auth_code = flask.request.args.get('code')
+        credentials = flow.step2_exchange(auth_code)
+        open('credentials.json','w').write(credentials.to_json()) # write access token to credentials.json locally 
+        return flask.redirect(flask.url_for('index'))
 
 def get_credentials():
 	credential_path = 'credentials.json'
